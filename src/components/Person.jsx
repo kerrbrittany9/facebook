3cf90aace9304cd64a0f2f7e975eaf2ba4df6140
Brittany Kerr and Nicole Freed <brittany@>
2017-09-12 10:02:19 -0700

    style Person component

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -3,15 +3,30 @@ import PropTypes from "prop-types";
 
 function Person(props) {
   var imgStyle = {
-    width: '30%'
+    width: '100%'
   }
+
+  var statsContainerStyle = {
+    display: 'grid',
+    gridTemplateColumns: '30% 30% 30%'
+  }
+
+  var statsStyle = {
+    backgroundColor: '#dedede',
+    border: '1px solid #ffffff',
+    padding: '.5rem',
+    fontSize: '13px'
+  }
+
   return (
     <div>
       <img style={imgStyle} src={props.image}/>
       <h2>{props.name}</h2>
-      <h4>Tweets</h4>
-      <h4>Followers</h4>
-      <h4>Following</h4>
+      <div style={statsContainerStyle}>
+        <h4 style={statsStyle}>Tweets: 500</h4>
+      <h4 style={statsStyle}>Followers: 1</h4>
+    <h4 style={statsStyle}>Following: 1000</h4>
+      </div>
     </div>
   );
 }
