e18656dec5dfe912dee979790696a63fc94f6f18
Brittany Kerr and Nicole Freed <nicole@>
2017-09-12 09:34:10 -0700

    style header element

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,27 +2,38 @@ import React from "react";
 
 function Header(props) {
 
+var headerStyle = {
+    display: 'grid',
+    gridTemplateColumns: '7% 7% 7% 60% 7.5% 7.5%',
+    marginBottom: '2rem'
+  }
+
   var homeStyle = {
     border: '1px solid grey',
-    float: 'left',
-    padding: '5px'
+    padding: '10px 5px 0 5px',
+    textAlign: 'center'
   }
   var notificationStyle = {
     border: '1px solid grey',
-    float: 'left',
-    padding: '5px'
+    padding: '10px 5px 0 5px',
+    textAlign: 'center'
   }
   var messageStyle = {
     border: '1px solid grey',
-    float: 'left',
-    padding: '5px'
+    padding: '10px 5px 0 5px',
+    textAlign: 'center'
   }
+
+  var logoStyle = {
+    justifySelf: 'center'
+  }
+
   return (
-    <div>
-      <h3 style={homeStyle}>Home</h3>
-      <h3 style={notificationStyle}>Notifications</h3>
-      <h3 style={messageStyle}>Messages</h3>
-      <h1>FaceClone</h1>
+    <div style={headerStyle}>
+      <p style={homeStyle}>Home</p>
+      <p style={notificationStyle}>Notifications</p>
+      <p style={messageStyle}>Messages</p>
+      <h1 style={logoStyle}>FaceClone</h1>
       <h3>Search</h3>
       <h3>Tweet</h3>
     </div>
